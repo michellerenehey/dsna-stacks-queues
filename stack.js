aaa603149154cc4fe65cfec70e9f3fb4ecfd11ad
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-04-05 12:56:28 -0700

    Include pseudocode to reference later

--- stack.js
+++ stack.js
@@ -1,21 +1,27 @@
 class Stack {
+  // initiating a constructor & setting an empty array
   constructor() {
     this.banana = [];
   }
 
+  // push an item onto stack
   push(item) {
-    return this.banana.push(item);
+    return this.banana.unshift(item);
   }
 
+  // pop off item from stack
+  // if using .pop need to use unshift in push
+  // could also use .push paired with .shift
   pop() {
     return this.banana.pop() || null;
   }
 
+  // see if there are items left in the array
   peek() {
     return this.banana[this.length - 1];
   }
 
-  // also works:
+  // also works (with latest version):
   // peek() {
   //   return this.banana.at(-1)
   // }
