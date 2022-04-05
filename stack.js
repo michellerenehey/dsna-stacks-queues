c7b87b515b39e70d930ca988cc6e6b041182f885
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-04-05 11:42:35 -0700

    Changed 'stack' to 'banana' to remind myself that is a variable

--- stack.js
+++ stack.js
@@ -1,24 +1,28 @@
 class Stack {
   constructor() {
-    this.stack = [];
+    this.banana = [];
   }
 
   push(item) {
-    return this.stack.push(item);
+    return this.banana.push(item);
   }
 
   pop() {
-    return this.stack.pop() || null;
+    return this.banana.pop() || null;
   }
 
   peek() {
-    return this.stack[this.length - 1];
+    return this.banana[this.length - 1];
   }
 
   get length() {
-    return this.stack.length;
+    return this.banana.length;
   }
 }
+
+// class Stack {
+//   #list = []; // this is NEW! putting properties on classes directly. # is private.
+// }
 
 const stack = new Stack();
 stack.push('fox');
